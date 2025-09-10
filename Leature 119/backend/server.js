5dc1e83a19761075e7ab06522f357359edfcfe8a
Ramnath <ramnath2544@gmail.com>
2025-09-10 14:00:33 +0530

    Handling  the forms with react-hook-form

---
+++ Leature 119/backend/server.js
@@ -0,0 +1,15 @@
+import express from "express"
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+  res.send(req.form)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ Leature 119/src/App.jsx
@@ -0,0 +1,63 @@
+import "./App.css";
+import { useForm } from "react-hook-form";
+
+function App() {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const delay = (d) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, d * 1000);
+    });
+  };
+
+  const onSubmit = async (data) => {
+    await delay(2); // simulating network delay
+    console.log(data);
+  //   if(data.username !== "shubham"){
+  //     setError("myform",{message: "Your form is not in good order because credentials are invalid!"})
+  //   }
+  //  if(data.username === "rohan"){
+  //     setError("blocked", {message: "Sorry this user is blocked"})
+  //   }
+  };
+
+  return (
+    <>
+      {isSubmitting && <div>Loading...</div>}
+      <div className="container">
+        <form action="" onSubmit={handleSubmit(onSubmit)}>
+          <input
+            placeholder="username"
+            {...register("username", {
+              required: { value: true, message: "This field is required." },
+              minLength: { value: 3, message: "Min length is 3" },
+              maxLength: { value: 8, message: "Max length is 8" },
+            })}
+            type="text"
+          />
+          {errors.username && <div>{errors.username.message}</div>}
+          <br />
+          <input
+            placeholder="password"
+            {...register("password", {minLength: { value: 7, message: "Min length of password is 7"},})}
+            type="password"
+          />
+          {errors.password && <div>{errors.password.message}</div>}
+          <br />
+          <input disabled={isSubmitting} type="submit" value="Submit" />
+          {errors.myform && <div>{errors.myform.message}</div>}
+          {errors.blocked && <div>{errors.blocked.message}</div>}
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default App;
